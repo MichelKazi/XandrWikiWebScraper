6ccffefd2b96b6ac53c2ed37b2d3b6c081b4d3bc
Michelkazi <michel.m.kazi@gmail.com>
2020-03-03 13:25:42 -0500

    added a port

--- index.js
+++
@@ -1,8 +0,0 @@
-const cheerio = require('cheerio')
-const axios = require('axios')
-const siteUrl = "https://remoteok.io/"
-
-const fetchData = async () => {
-	const result = await axios.get(siteUrl);
-	return cheerio.load(result.data);
-}

bedd8f123c861dcdc21535257d93f7a8aefcb8eb
Michelkazi <michel.m.kazi@gmail.com>
2020-03-04 14:40:37 -0500

    Colly package accesses the enpoint I need :)

--- colly/api/scraper.go
+++ colly/api/scraper.go
@@ -1,7 +1,32 @@
-package api
+package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	c := colly.NewCollector(
+		// Only visit domains allowed
+		colly.AllowedDomains("wiki.xandr.com", "wiki.appnexus.com"),
+	)
+
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		// Print out the link
+		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+
+		// Visit link found
+		// Since AllowedDomains were set, only visiting expected links
+		c.Visit(e.Request.AbsoluteURL(link))
+	})
+
+	// Log out each site being visited
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Now Visiting", r.URL.String())
+	})
+
+	c.Visit("https://wiki.xandr.com/")
+
 }
